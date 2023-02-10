48b7af5a44601facf7e534bfe009dae84399e248
Andrey <antonov.andrey.10101@gmail.com>
2023-02-10 11:36:25 +0300

    feat: add emoticons import

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,11 +1,38 @@
+import { useEffect, useState } from "react";
+import emojiGroups from "/public/emoji/emoji";
+import IEmojiGroup from "/src/types/types";
 import "./Form.scss";
 
+type visibilityType = "visible" | "hidden" | "collapse";
+
 function Form() {
+  const [emojiListVisibility, setEmojiListVisibility] =
+    useState<visibilityType>("hidden");
+
+  const [emojiList, setEmojiList] = useState<IEmojiGroup[]>([]);
+
+  useEffect(() => {
+    setEmojiList(emojiGroups);
+  }, []);
+
   return (
     <form className="app__form">
-      <textarea
-        className="app__textarea"
-        placeholder="Ваше сообщение"
+      <textarea className="app__textarea" placeholder="Ваше сообщение" />
+      <div
+        className="app__emoji-list"
+        style={{
+          visibility: `${emojiListVisibility}`,
+        }}
+      >
+        {emojiList.map((item, index) => (
+          <span key={index}> '1'</span>
+        ))}
+      </div>
+      <img
+        className="app__emoji-icon"
+        onMouseOver={() => setEmojiListVisibility("visible")}
+        onMouseLeave={() => setEmojiListVisibility("hidden")}
+        src="/public/svg/emoji-icon.svg"
       />
     </form>
   );
---
+++ src/types/types.ts
@@ -0,0 +1,4 @@
+export interface IEmojiGroup {
+  title: string;
+  items: string[];
+}
